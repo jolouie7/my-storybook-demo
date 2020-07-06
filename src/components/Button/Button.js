1489590f074f33459549c9b4de88bdc5d6ec6eb7
Your_Name <joseph161216@gmail.com>
2020-07-06 02:37:09 -0700

    add prop disabled

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,19 +1,49 @@
 import React from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const BaseButton = styled.button`
   border-radius: 4px;
   cursor: pointer;
   font-size: 1rem;
   padding: 9px 36px;
-`
+`;
 
-const Button = ({ children, ...rest }) => {
+// const Button = ({ children, ...rest }) => {
+//   return (
+//     <BaseButton className="button" {...rest}>
+//       {children}
+//     </BaseButton>
+//   );
+// };
+
+// Extend BaseButton
+const Button = styled(BaseButton)`
+  background: ${(props) => props.theme.primary};
+  border: none;
+  color: #fff
+
+  :hover,
+  :focus {
+    background-color: ${(props) => props.theme.primaryHover};
+    color: #fff;
+  }
+
+  // Using Props
+  ${(props) =>
+    props.disabled &&
+    `
+    opacity: 0.50;
+    cursor: not-allowed;
+  `}
+`;
+
+const ButtonComponent = () => {
   return (
-    <BaseButton className="button" {...rest}>
-      {children}
-    </BaseButton>
+    <>
+      <Button>{"Hello World!"}</Button>
+      <Button disabled>{"Hello World!"}</Button>
+    </>
   );
 };
 
-export default Button;
+export default ButtonComponent;
